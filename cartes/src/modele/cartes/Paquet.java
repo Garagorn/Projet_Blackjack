6fce0d0a2cf9cf5ceb75bff93aa3026827bb5e94
Siaghi <siaghimassinissa@gmail.com>
2025-10-09 12:24:51 +0200

    mon deuxième dépot

---
+++ cartes/src/modele/cartes/Paquet.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modele.cartes;
+ import modele.event.*;
+/**
+ *
+ * @author siaghi231
+ */
+public class Paquet extends AbstractModeleEcoutable {
+    
+}
