36d22d1b2220d31debcefabb529d2e9990f8df98
Basile Tellier <basile.tellier@uni-caen.fr>
2025-10-09 12:27:32 +0200

    Nettoyage des dossiers

--- blackjack/dist/docs/api/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"blackjack","c":"MainClass","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"blackjack","c":"MainClass","l":"MainClass()","u":"%3Cinit%3E()"}];updateSearchResults();
